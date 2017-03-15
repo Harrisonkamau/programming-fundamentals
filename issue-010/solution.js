dcf0f5420710f68b4622c0d0c3d5cba542d991eb
Anthony Nandaa <profnandaa@gmail.com>
2017-03-15 04:17:47 +0300

    solution: issue #10 (bonus)

--- issue-010/solution.js
+++ issue-010/solution.js
@@ -1,4 +1,14 @@
-// implement your solution here
 function evenOdd(a) {
-
+    var sum = 0;
+    a.reduce(function(x, y) {
+        if (y % 2 === 0) {
+            // since the beginning accumulator (x)
+            // will be 0
+            sum += y;
+        } else {
+            // subtract from the total
+            sum -= y;
+        }
+    }, 0);
+    return sum;
 }
