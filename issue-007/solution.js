d40ae3715833967218c62a95924881d8703f9a6e
Anthony Nandaa <profnandaa@gmail.com>
2017-03-15 04:08:31 +0300

    solution: issue #7

--- issue-007/solution.js
+++ issue-007/solution.js
@@ -1,4 +1,13 @@
-// implement your solution here
-function superSum() {
-
+function superSum(a) {
+    var sum = 0;
+    a.forEach(function(number) {
+        number
+            .toString() // so we can split into each digit
+            .split('') // convert to array so we can use the .forEach method
+            .forEach(function(digit) {
+                // convert back to integer so we can sum up
+                sum += parseInt(digit, 10);
+            });
+    });
+    return sum;
 }
