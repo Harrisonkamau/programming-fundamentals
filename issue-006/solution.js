fc96ce5a0a08900f28bd573baae9fa61eec3d9a0
Anthony Nandaa <profnandaa@gmail.com>
2017-03-15 04:04:37 +0300

    solution: issue #6

--- issue-006/solution.js
+++ issue-006/solution.js
@@ -1,4 +1,10 @@
 // implement your solution here
-function selectSum() {
-
+function selectSum(a) {
+    var sum = 0;
+    a.forEach(function(item) {
+        if (!isNaN(item) && typeof item === 'number') {
+            sum += item;
+        }
+    });
+    return sum;
 }
