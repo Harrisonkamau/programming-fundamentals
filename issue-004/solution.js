308d3b7676a9ac6a03840868e49b233f8d5393dd
Anthony Nandaa <profnandaa@gmail.com>
2017-03-15 03:59:36 +0300

    solution: issue #4

--- issue-004/solution.js
+++ issue-004/solution.js
@@ -1,3 +1,16 @@
 function grade(a) {
-    // implement your solution here
+    // assumption, all marks are btn 0 and 100
+    if (a >= 80) {
+        return 'A';
+    } else if (a >= 60) {
+        return 'B';
+    } else if (a >= 50) {
+        return 'C';
+    } else if (a >= 40) {
+        return 'D';
+    } else if (a >= 30) {
+        return 'E';
+    } else {
+        return 'F';
+    }
 }
