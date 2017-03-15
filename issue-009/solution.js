ccc38a111e7bc50a1da9f3705dd506a331a9be79
Anthony Nandaa <profnandaa@gmail.com>
2017-03-15 04:16:15 +0300

    solution: issue #9

--- issue-009/solution.js
+++ issue-009/solution.js
@@ -1,4 +1,11 @@
-// implement your solution here
 function sumEven(a) {
-
+    var sum = 0;
+    a.reduce(function(x, y) {
+        if (y % 2 === 0) {
+            // since the beginning accumulator (x)
+            // will be 0
+            sum += y;
+        }
+    }, 0);
+    return sum;
 }
