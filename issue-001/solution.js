320ce13e61fd0a3d1ba1c74cf5e82de7f3246030
Anthony Nandaa <profnandaa@gmail.com>
2017-03-15 03:52:16 +0300

    solution: issue #1

--- issue-001/solution.js
+++ issue-001/solution.js
@@ -1,3 +1,30 @@
 function dataTypeCheck(a) {
-    // implement your solution here
+    if (typeof a === 'number') {
+        // are numbers, either NaN, int or float
+        if (isNaN(a)) {
+            return 0;
+        } else if (a % 1 === 0) {
+            // integer
+            return a / 2;
+        } else {
+            // float
+            return a * 2;
+        }
+    } else if (typeof a === 'string') {
+        // check if it can be typecast
+        if (!isNaN(parseInt(a))) {
+            // check if it's float or integer
+            if (a.indexOf('.') != -1) {
+                // it's float
+                return parseFloat(a);
+            }
+            // else, int
+            return parseInt(a, 10);
+        } else {
+            // any other string
+            return 'hello ' + a;
+        }
+    } else if (a === null || a === undefined) {
+        return 0;
+    }
 }
