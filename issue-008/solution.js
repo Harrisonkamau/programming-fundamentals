8f4ba7c49e553a1261b73be248749b30d9809dc3
Anthony Nandaa <profnandaa@gmail.com>
2017-03-15 04:11:09 +0300

    solution: issue #8

--- issue-008/solution.js
+++ issue-008/solution.js
@@ -1,4 +1,8 @@
-// implement your solution here
 function sum() {
-
+    var sum = 0;
+    Array.from(arguments) // borrowed from Mirriam (@Chanya-M)
+        .forEach(function(number) {
+            sum += number;
+        });
+    return sum;
 }
