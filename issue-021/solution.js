312c4612001fc79707bff6b0baba3b86992a954b
Anthony Nandaa <profnandaa@gmail.com>
2017-03-16 13:36:52 +0300

    solution: issue #21

--- issue-021/solution.js
+++ issue-021/solution.js
@@ -1,4 +1,17 @@
 // implement your solution here
 function oldAndYoung(students) {
-
+    var y = 1800,
+        o = 2017,
+        yO = {};
+    students.forEach(function(student) {
+        if (student.yearOfBirth > y) {
+            yO.youngest = student.name;
+            y = student.yearOfBirth;
+        }
+        if (student.yearOfBirth < o) {
+            yO.oldest = student.name;
+            o = student.yearOfBirth;
+        }
+    });
+    return yO;
 }
